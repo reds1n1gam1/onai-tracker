6f7daf71d362bcce00e596dc3d4bfd8e89df06a4
Dima <reds1n1gam1@gmail.com>
2026-05-28 02:56:01 +0300

    feat: mock image update

--- app/components/landing/MainBanner.vue
+++ app/components/landing/MainBanner.vue
@@ -7,7 +7,9 @@
             <Button>See how it works</Button>
         </div>
 
-        <img src="https://dummyimage.com/1200x800/cccccc/fff.jpg" alt="">
+        <img src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+            alt="Photo by Drew Beamer" fill
+            class="h-full w-full rounded-lg object-cover dark:brightness-[0.2] dark:grayscale">
 
     </div>
 </template>
