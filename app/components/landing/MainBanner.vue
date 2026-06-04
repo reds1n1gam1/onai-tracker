5c7c7e801608e44f8584a13b51fc532fb0b22623
Dima <reds1n1gam1@gmail.com>
2026-06-04 22:05:02 +0300

    feat: landing page changes

--- app/components/landing/MainBanner.vue
+++ app/components/landing/MainBanner.vue
@@ -1,23 +1,34 @@
 <template>
-    <div class="landing-banner">
-        <h1 class="landing-banner__title">Slogan For Main banner</h1>
-        <h2 class="landing-banner__description">Description text</h2>
-        <div class="landing-banner__actions">
-            <Button>Start tracking time free</Button>
-            <Button>See how it works</Button>
+    <div class="landing-banner grid md:grid-cols-2 sm:grid-cols-1">
+        <div class="flex flex-col gap-6">
+            <div class="flex gap-2 flex-col">
+                <h1 class="text-6xl font-bold">Track you time</h1>
+                <h1 class="text-6xl font-bold">Focus on <span class="accent-text">what matters</span></h1>
+            </div>
+            <h2 class="text-xl">Priority Time Tracker helps you organize tasks, track work sessions, and
+                automatically understand what deserves your attention next</h2>
+            <div class="flex flex-row gap-4">
+                <Button size="lg" class=" text-lg p-6">Start tracking time free</Button>
+                <Button size="lg" variant="secondary" class=" p-6 text-lg">See how it works</Button>
+            </div>
         </div>
 
-        <img src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-            alt="Photo by Drew Beamer" fill
-            class="h-full w-full rounded-lg object-cover dark:brightness-[0.2] dark:grayscale">
+        <div>
+            <img src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+                alt="Photo by Drew Beamer" fill
+                class="h-full w-full rounded-lg object-cover dark:brightness-[0.2] dark:grayscale">
+        </div>
+
 
     </div>
 </template>
 
 <script setup lang="ts">
 import Button from '../ui/button/Button.vue';
-
-
 </script>
 
-<style scoped></style>
+<style scoped>
+.accent-text {
+    color: oklch(0.58 0.23 260.9);
+}
+</style>
