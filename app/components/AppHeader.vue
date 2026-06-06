7d3ef132ef23449f093dfb6d509eaf0260b08ea1
Dima <reds1n1gam1@gmail.com>
2026-06-06 22:52:30 +0300

    feat: added header and other pages templates

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,13 +1,25 @@
 <template>
-    <div>
-        Header
+    <div class="shadow-md bg-white flex flex-row justify-between p-6">
+        <p class="text-gray-700 text-lg font-medium">Dashboard</p>
+
+        <div class="flex flex-row gap-2">
+            <button>
+                <IconBell />
+            </button>
+
+            <Avatar>
+                <AvatarImage src="https://github.com/evilrabbit.png" alt="@evilrabbit" />
+                <AvatarFallback>ER</AvatarFallback>
+            </Avatar>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-
+import { IconBell } from '@tabler/icons-vue';
+import Avatar from './ui/avatar/Avatar.vue';
+import AvatarImage from './ui/avatar/AvatarImage.vue';
+import AvatarFallback from './ui/avatar/AvatarFallback.vue';
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
