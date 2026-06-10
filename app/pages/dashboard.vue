5d66c9f36c754a05786dfe3ac24a625248ccb9eb
reds1n1gam1 <reds1n1gam1@gmail.com>
2026-06-10 20:15:41 +0500

    feat: database api updates

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="grid gap-6">
+        <ul v-if="users?.length">
+            <li v-for="user in users" :key="user.id">{{ user.name }} ({{ user.email }})</li>
+        </ul>
+
         <DashboardTimer />
 
         <DashboardQuickTaskAdd />
@@ -7,14 +11,14 @@
         <div class="grid grid-cols-4 gap-6">
             <DashboardTodayTasksList class="col-span-3" />
 
-            <DashboardRecommendedTask  />
+            <DashboardRecommendedTask />
         </div>
 
     </div>
 </template>
 
 <script setup>
-
+const { data: users } = await useFetch('/api/users')
 </script>
 
 <style lang="scss" scoped></style>
