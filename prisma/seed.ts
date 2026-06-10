8e434005bff3196015890a1dbf53d9d16d829b11
reds1n1gam1 <43748239+reds1n1gam1@users.noreply.github.com>
2026-06-11 01:17:02 +0500

    feat: added hash password for login and registration endpoints (#30)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,21 +6,21 @@ const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
-  const createMany = await prisma.user.createMany({
-  data: [
-    {
-      name: "Admin",
-      email: "admin@admin.com",
-      password: "Qwerty12345",
-    },
-    {
-      name: "John Doe",
-      email: "jogn@gmail.com",
-      password: "Qwerty12345",
-    },
-  ],
-  skipDuplicates: true, // Skip records with duplicate unique fields
-});
+//   const createMany = await prisma.user.createMany({
+//   data: [
+//     {
+//       name: "Admin",
+//       email: "admin@admin.com",
+//       password: "Qwerty12345",
+//     },
+//     {
+//       name: "John Doe",
+//       email: "jogn@gmail.com",
+//       password: "Qwerty12345",
+//     },
+//   ],
+//   skipDuplicates: true, // Skip records with duplicate unique fields
+// });
 
 }
 
