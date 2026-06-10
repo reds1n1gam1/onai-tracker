2f20dec0623a3f398e7e1711828c652fe7df3034
reds1n1gam1 <reds1n1gam1@gmail.com>
2026-06-10 20:17:46 +0500

    feat: fixed prisma error

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../prisma/generated/client";
+import { PrismaClient } from "../../prisma/generated-client/client";
 
 const prismaClientSingleton = () => {
   const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
