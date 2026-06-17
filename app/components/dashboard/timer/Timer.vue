a0ced490038aef73581fdce7bf72670053ff5037
reds1n1gam1 <reds1n1gam1@gmail.com>
2026-06-17 15:38:20 +0500

    feat: added timer color update

--- app/components/dashboard/timer/Timer.vue
+++ app/components/dashboard/timer/Timer.vue
@@ -6,7 +6,13 @@
 
     <div class="flex flex-row justify-center items-start">
       <div class="flex flex-col justify-start items-center w-20">
-        <p class="text-center text-7xl text-blue-500 font-semibold">
+        <p
+          class="text-center text-7xl font-semibold"
+          :class="{
+            'text-blue-500': store.currentState === 'running',
+            'text-gray-500': store.currentState !== 'paused',
+          }"
+        >
           {{ secondsToDate(elapsedTime) || "00:00:00" }}
         </p>
         <div
